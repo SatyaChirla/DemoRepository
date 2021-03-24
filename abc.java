b20340b9bd834047bf125df3046acbbd4b758204
SatyaChirla <ch.satyakrishnasri999@gmail.com>
2021-03-24 18:54:43 +0530

    Altered abc.java and Constructed Atoz.java and xyz.java

--- abc.java
+++ abc.java
@@ -3,5 +3,9 @@ public class abc
 	public static void main(String args[])
 	{
 		System.out.println("HELLO");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Hi");
+		}
 	}
 }
---
+++ xyz.java
@@ -0,0 +1,16 @@
+public class xyz
+{
+	private int x;
+	private String str;
+	public void setStr()
+	{	
+	}
+	public void setX()
+	{	
+	}
+	public int getX()
+	{	
+		return x;
+	}
+	
+}
